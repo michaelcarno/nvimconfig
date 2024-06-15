a3e1c7bfb00a2bda28ddea44872afccce4629b8c
Michael Carno <bondarenko87@gmail.com>
2024-06-15 22:53:12 +0700

    Chaged: skip simple confirm for oil

--- lua/user/plugins/oil.lua
+++ lua/user/plugins/oil.lua
@@ -34,9 +34,9 @@ return {
         concealcursor = "nvic",
       },
       -- Send deleted files to the trash instead of permanently deleting them (:help oil-trash)
-      delete_to_trash = false,
+      delete_to_trash = true,
       -- Skip the confirmation popup for simple operations
-      skip_confirm_for_simple_edits = false,
+      skip_confirm_for_simple_edits = true,
       -- Change this to customize the command used when deleting to trash
       trash_command = "trash-put",
       -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
